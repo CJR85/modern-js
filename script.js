3be0ac940619b70c2295b11fe0b1fc5a56649e40
Calvin Roberts <roberts.calvin85@gmail.com>
2021-02-02 18:44:52 +0000

    Intro to NPM.

--- script.js
+++ script.js
@@ -23,6 +23,7 @@
 //////////////////////////////////////////////
 //////////////////////////////////////////////
 
+/*
 const ShoppingCart2 = (function () {
   const cart = [],
     shippingCost = 10,
@@ -53,3 +54,31 @@ ShoppingCart2.addToCart('apple', 4);
 ShoppingCart2.addToCart('pizza', 2);
 console.log(ShoppingCart2);
 console.log(ShoppingCart2.shippingCost);
+*/
+
+/*
+// Export
+export.addToCart = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(
+      `${quantity} ${product} added to cart (shipping cost is ${shippingCost})`
+    );
+  };
+*/
+
+import cloneDeep from './node_modules/lodash-es/cloneDeep.js';
+
+const state = {
+  cart: [
+    { product: 'bread', quantity: 5 },
+    { product: 'pizza', quantity: 5 },
+  ],
+  user: { loggedIn: true },
+};
+const stateClone = Object.assign({}, state);
+const stateDeepClone = cloneDeep(state);
+
+state.user.loggedIn = false;
+console.log(stateClone);
+
+console.log(stateDeepClone);
