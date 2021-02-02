4b99e78e827d37e5d916b1fbafc21cfcf3be4474
Calvin Roberts <roberts.calvin85@gmail.com>
2021-02-02 18:03:46 +0000

    Module Pattern.

--- script.js
+++ script.js
@@ -3,18 +3,53 @@
 // addToCart('bread', 5);
 // console.log(price, tq);
 
-console.log('Importing module');
-// console.log(shippingCost);
+// console.log('Importing module');
+// // console.log(shippingCost);
 
-// import * as ShoppingCart from './shoppingCart.js';
-// ShoppingCart.addToCart('bread', 5);
-// console.log(ShoppingCart.totalPrice);
-// import add, { addToCart, totalPrice as price, tq } from './shoppingCart.js';
-// console.log(price);
+// // import * as ShoppingCart from './shoppingCart.js';
+// // ShoppingCart.addToCart('bread', 5);
+// // console.log(ShoppingCart.totalPrice);
+// // import add, { addToCart, totalPrice as price, tq } from './shoppingCart.js';
+// // console.log(price);
 
-import add, { cart } from './shoppingCart.js';
-add('pizza', 2);
-add('bread', 5);
-add('apples', 4);
+// import add, { cart } from './shoppingCart.js';
+// add('pizza', 2);
+// add('bread', 5);
+// add('apples', 4);
 
-console.log(cart);
+// console.log(cart);
+
+//////////////////////////////////////////////
+//////////////////////////////////////////////
+//////////////////////////////////////////////
+
+const ShoppingCart2 = (function () {
+  const cart = [],
+    shippingCost = 10,
+    totalPrice = 237,
+    totalQuantity = 23;
+
+  const addToCart = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(
+      `${quantity} ${product} added to cart (shipping cost is ${shippingCost})`
+    );
+  };
+
+  const orderStock = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(`${quantity} ${product} ordered from supplier`);
+  };
+
+  return {
+    addToCart,
+    cart,
+    totalPrice,
+    totalQuantity,
+  };
+})();
+
+ShoppingCart2.addToCart('apple', 4);
+ShoppingCart2.addToCart('pizza', 2);
+console.log(ShoppingCart2);
+console.log(ShoppingCart2.shippingCost);
